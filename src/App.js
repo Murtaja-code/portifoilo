dd3eda734612d9e43f437267dd97250a1aa80453
Murtaja Adnan <murtaja07805@gmail.com>
2021-04-25 22:26:05 +0300

    Feat: create the structure and add dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-tsparticles"
+import particlesConfig from "./particle-config"
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Particles
+				id="tsparticles"
+				style={{
+					position: "fixed",
+					top: 0,
+					left: 0,
+					width: "100%",
+					height: "100%"
+				}}
+				params={particlesConfig}
+			/>
+		</div>
+	)
 }
 
-export default App;
+export default App
