0e44d653cee4a79e5ab056757280791ea02bef26
Murtaja Adnan <murtaja07805@gmail.com>
2021-04-29 08:49:46 +0300

    Fix: delete comment

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -79,48 +79,6 @@ function Work() {
 							{projects.map((project, i) => (
 								<Grid item xs={12} sm={6} md={4} lg={3} key={i}>
 									{cards(project, true)}
-									{/* <Fade left delay={project.delay}>
-										<Card className={classes.card}>
-											<CardActionArea className="card">
-												<CardMedia component="img" src={project.img} alt="ll" />
-												<CardContent className="card">
-													<Typography gutterBottom variant="h5" component="h2">
-														{project.title}
-													</Typography>
-													<Typography variant="body2" color="textSecondary" component="p">
-														{project.descrption}
-													</Typography>
-												</CardContent>
-											</CardActionArea>
-											<CardActions>
-												<a href={project.link} className={classes.link}>
-													<Button variant="contained" size="small" color="primary">
-														Live
-													</Button>
-												</a>
-												<a href={project.repo} className={classes.link}>
-													<Button variant="contained" size="small" color="primary">
-														Repo
-													</Button>
-												</a>
-											</CardActions>
-											<div>
-												<Grid container justify="center" alignContent="center" className="hidden">
-													{project.tools.map((tool, i) => (
-														<Bounce key={i}>
-															<Button
-																className={classes.tools}
-																size="small"
-																color="primary"
-																variant="outlined">
-																{tool}
-															</Button>
-														</Bounce>
-													))}
-												</Grid>
-											</div>
-										</Card>
-									</Fade> */}
 								</Grid>
 							))}
 						</Grid>
