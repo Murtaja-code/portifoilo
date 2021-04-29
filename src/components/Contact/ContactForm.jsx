2d63f65487c4bb589a404b105a29c30aeb5f8306
Murtaja Adnan <murtaja07805@gmail.com>
2021-04-29 16:42:20 +0300

    Feat: add animation to contact

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -10,6 +10,8 @@ import {
 	ThemeProvider
 } from "@material-ui/core"
 import useStyles, { theme } from "./Style"
+import Fade from "react-reveal/Fade"
+import Bounce from "react-reveal/Bounce"
 
 function ContactForm({ handleChange, handleSubmit, values }) {
 	const classes = useStyles()
@@ -22,83 +24,102 @@ function ContactForm({ handleChange, handleSubmit, values }) {
 				<Box mt={3.3}>
 					<Box align="left" mb={1.3}>
 						<Typography component="h1" variant="h4" className={classes.title}>
-							Get in touch
+							<Bounce cascade left delay={500}>
+								Get in touch
+							</Bounce>
 						</Typography>
 						<Typography component="h3" variant="h6" className={classes.text}>
-							Would like to know more!
+							<Bounce cascade left delay={800}>
+								Would like to know more!
+							</Bounce>
 						</Typography>
-						<Typography variant="body2" className={classes.text}>
-							Interested in my work? <br />
-							Have a project that could use my skills?
-							<br />
-							Wanna hire me?
-							<br />
-							then please drop a message and let’s chat!
-							<br />
-							<strong>Email: </strong>murtajaadnan7@gmail.com
-						</Typography>
+						<Bounce cascade left delay={1000}>
+							<Typography variant="body2" className={classes.text}>
+								Interested in my work?
+								<br />
+								Have a project that could use my skills?
+								<br />
+								Wanna hire me?
+								<br />
+								then please drop a message and let’s chat!
+								<br />
+								<strong>Email: </strong>murtajaadnan7@gmail.com
+							</Typography>
+						</Bounce>
 					</Box>
+
 					<form onSubmit={handleSubmit}>
 						<Grid container spacing={2} direction="row" justify="space-around">
 							<Grid item xs={12} sm={6}>
-								<TextField
-									value={firstName}
-									onChange={(e) => handleChange(e)}
-									autoComplete="fname"
-									name="firstName"
-									variant="outlined"
-									placeholder="first name"
-									required
-									fullWidth
-									id="firstName"
-									autoFocus
-								/>
+								<Fade delay={1200} left>
+									<TextField
+										value={firstName}
+										onChange={(e) => handleChange(e)}
+										autoComplete="fname"
+										name="firstName"
+										variant="outlined"
+										placeholder="first name"
+										required
+										fullWidth
+										id="firstName"
+										autoFocus
+									/>
+								</Fade>
 							</Grid>
+
 							<Grid item xs={12} sm={6}>
-								<TextField
-									value={lastName}
-									onChange={(e) => handleChange(e)}
-									variant="outlined"
-									placeholder="last name"
-									required
-									fullWidth
-									id="lastName"
-									name="lastName"
-									autoComplete="lname"
-								/>
+								<Fade delay={1200} right>
+									<TextField
+										value={lastName}
+										onChange={(e) => handleChange(e)}
+										variant="outlined"
+										placeholder="last name"
+										required
+										fullWidth
+										id="lastName"
+										name="lastName"
+										autoComplete="lname"
+									/>
+								</Fade>
 							</Grid>
 							<Grid item xs={12}>
-								<TextField
-									value={email}
-									onChange={(e) => handleChange(e)}
-									variant="outlined"
-									placeholder="eamil"
-									required
-									fullWidth
-									id="email"
-									name="email"
-									autoComplete="email"
-								/>
+								<Fade delay={1400} left>
+									<TextField
+										value={email}
+										onChange={(e) => handleChange(e)}
+										variant="outlined"
+										placeholder="eamil"
+										required
+										fullWidth
+										id="email"
+										name="email"
+										autoComplete="email"
+									/>{" "}
+								</Fade>
 							</Grid>
 							<Grid item xs={12}>
-								<TextField
-									value={text}
-									onChange={(e) => handleChange(e)}
-									variant="outlined"
-									multiline
-									rowsMax={4}
-									required
-									fullWidth
-									placeholder="message"
-									name="text"
-									type="TextareaAutosize"
-									id="textarea"
-								/>
+								<Fade delay={1600} right>
+									<TextField
+										value={text}
+										onChange={(e) => handleChange(e)}
+										variant="outlined"
+										multiline
+										rowsMax={4}
+										required
+										fullWidth
+										placeholder="message"
+										name="text"
+										type="TextareaAutosize"
+										id="textarea"
+									/>
+								</Fade>
 							</Grid>
 							<Grid item xs={12}>
-								<Button type="submit" color="primary" variant="contained">
-									submit
-								</Button>
+								<Fade delay={1800} left>
+									<Button type="submit" color="primary" variant="contained">
+										submit
+									</Button>
+								</Fade>
 							</Grid>
 						</Grid>
 					</form>
