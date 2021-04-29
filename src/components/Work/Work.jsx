9e02504f77555f7a4c571cc3aeba953c3011d104
Murtaja Adnan <murtaja07805@gmail.com>
2021-04-29 08:49:13 +0300

    Feat: use Carousel for cards in mobile view

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -10,15 +10,60 @@ import {
 	Box,
 	Container,
 	CardMedia,
-	ThemeProvider
+	ThemeProvider,
+	useMediaQuery
 } from "@material-ui/core"
 import projects from "../../constData"
 import useStyles, { theme } from "./Style"
 import Bounce from "react-reveal/Bounce"
 import Fade from "react-reveal/Fade"
-
+import Carousel from "react-material-ui-carousel"
 function Work() {
 	const classes = useStyles()
+	const mobile = useMediaQuery("(max-width: 700px)")
+
+	const cards = (project, delay) => {
+		return (
+			<Fade left delay={delay ? project.delay : 0}>
+				<Card className={classes.card}>
+					<CardActionArea className="card">
+						<CardMedia component="img" src={project.img} alt="ll" />
+						<CardContent className="card">
+							<Typography gutterBottom variant="h5" component="h2">
+								{project.title}
+							</Typography>
+							<Typography variant="body2" color="textSecondary" component="p">
+								{project.descrption}
+							</Typography>
+						</CardContent>
+					</CardActionArea>
+					<CardActions>
+						<a href={project.link} className={classes.link}>
+							<Button variant="contained" size="small" color="primary">
+								Live
+							</Button>
+						</a>
+						<a href={project.repo} className={classes.link}>
+							<Button variant="contained" size="small" color="primary">
+								Repo
+							</Button>
+						</a>
+					</CardActions>
+					<div>
+						<Grid container justify="center" alignContent="center" className="hidden">
+							{project.tools.map((tool, i) => (
+								<Bounce key={i}>
+									<Button className={classes.tools} size="small" color="primary" variant="outlined">
+										{tool}
+									</Button>
+								</Bounce>
+							))}
+						</Grid>
+					</div>
+				</Card>
+			</Fade>
+		)
+	}
 	return (
 		<Container>
 			<ThemeProvider theme={theme}>
@@ -29,54 +74,63 @@ function Work() {
 						</Bounce>
 					</Typography>
 
-					<Grid container spacing={3} justify="center" alignItems="center">
-						{projects.map((project, i) => (
-							<Grid item xs={12} sm={6} md={4} lg={3} key={i}>
-								<Fade left delay={project.delay}>
-									<Card className={classes.card}>
-										<CardActionArea className="card">
-											<CardMedia component="img" src={project.img} alt="ll" />
-											<CardContent className="card">
-												<Typography gutterBottom variant="h5" component="h2">
-													{project.title}
-												</Typography>
-												<Typography variant="body2" color="textSecondary" component="p">
-													{project.descrption}
-												</Typography>
-											</CardContent>
-										</CardActionArea>
-										<CardActions>
-											<a href={project.link} className={classes.link}>
-												<Button variant="contained" size="small" color="primary">
-													Live
-												</Button>
-											</a>
-											<a href={project.repo} className={classes.link}>
-												<Button variant="contained" size="small" color="primary">
-													Repo
-												</Button>
-											</a>
-										</CardActions>
-										<div>
-											<Grid container justify="center" alignContent="center" className="hidden">
-												{project.tools.map((tool, i) => (
-													<Bounce key={i}>
-														<Button
-															className={classes.tools}
-															size="small"
-															color="primary"
-															variant="outlined">
-															{tool}
-														</Button>
-													</Bounce>
-												))}
-											</Grid>
-										</div>
-									</Card>
-								</Fade>
-							</Grid>
-						))}
-					</Grid>
+					{!mobile ? (
+						<Grid container spacing={3} justify="center" alignItems="center">
+							{projects.map((project, i) => (
+								<Grid item xs={12} sm={6} md={4} lg={3} key={i}>
+									{cards(project, true)}
+									{/* <Fade left delay={project.delay}>
+										<Card className={classes.card}>
+											<CardActionArea className="card">
+												<CardMedia component="img" src={project.img} alt="ll" />
+												<CardContent className="card">
+													<Typography gutterBottom variant="h5" component="h2">
+														{project.title}
+													</Typography>
+													<Typography variant="body2" color="textSecondary" component="p">
+														{project.descrption}
+													</Typography>
+												</CardContent>
+											</CardActionArea>
+											<CardActions>
+												<a href={project.link} className={classes.link}>
+													<Button variant="contained" size="small" color="primary">
+														Live
+													</Button>
+												</a>
+												<a href={project.repo} className={classes.link}>
+													<Button variant="contained" size="small" color="primary">
+														Repo
+													</Button>
+												</a>
+											</CardActions>
+											<div>
+												<Grid container justify="center" alignContent="center" className="hidden">
+													{project.tools.map((tool, i) => (
+														<Bounce key={i}>
+															<Button
+																className={classes.tools}
+																size="small"
+																color="primary"
+																variant="outlined">
+																{tool}
+															</Button>
+														</Bounce>
+													))}
+												</Grid>
+											</div>
+										</Card>
+									</Fade> */}
+								</Grid>
+							))}
+						</Grid>
+					) : (
+						<Carousel>
+							{projects.map((project, i) => (
+								<span key={i}>{cards(project, false)}</span>
+							))}
+						</Carousel>
+					)}
 				</Box>
 			</ThemeProvider>
 		</Container>
