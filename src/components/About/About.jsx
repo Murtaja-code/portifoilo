a67c2580610577d7026886f55fa7e2876811f654
Murtaja <murtajaadnan7@gmail.com>
2021-05-06 01:05:36 +0300

    Fix: reduce the time for delay in about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,7 +21,7 @@ function About() {
 		<Container>
 			<Box pt={10}>
 				{/* languages*/}
-				{topText("Languages I speak", 100)}
+				{topText("Languages I speak", 50)}
 				<Grid container justify="center" spacing={2}>
 					{languages.map((icon, i) => (
 						<Grid key={i} item xs={6} sm={4} md={3} className={classes.scale}>
@@ -48,7 +48,7 @@ function About() {
 					))}
 				</Grid>
 				{/* frameworks */}
-				<Box mt={4}>{topText("Tools I use", 600)}</Box>
+				<Box mt={4}>{topText("Tools I use", 430)}</Box>
 				<Grid container justify="center" spacing={2}>
 					{frameworks.map((icon, i) => (
 						<Grid key={i} item xs={6} sm={4} md={3} className={classes.scale}>
